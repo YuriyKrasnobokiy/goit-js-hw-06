d2913d614d332fbc49e80cfb27756ffe6cdcda96
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2023-07-28 23:08:49 +0300

    add task 8

--- js/task-07.js
+++ js/task-07.js
@@ -3,10 +3,11 @@ const inputRage = document.querySelector("#font-size-control");
 const spanText = document.querySelector("#text");
 
 spanText.style.fontSize = `${inputRage.value}px`;
+
 // Додав зразу зміну розміру тексту спану бо при завантаженні сторінки розмір тексту спану не відповідає дефолтному значенню інпута (50%). Тільки потягнувши його змінюється розмір тексту.
 
 inputRage.addEventListener("input", handlerRage);
 
-function handlerRage() {
-  spanText.style.fontSize = `${inputRage.value}px`;
+function handlerRage(evt) {
+  spanText.style.fontSize = `${evt.currentTarget.value}px`;
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill all the fields!");
+  }
+
+  let result = {
+    email: email.value,
+    password: password.value,
+  };
+  console.dir(result);
+
+  event.currentTarget.reset();
+}
