949e61fb5b2880f667c138d614371e04af93811f
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2023-07-29 20:58:06 +0300

    add half of task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const elements = {
+  divControls: document.querySelector("#controls"),
+  buttons: document.querySelectorAll("button"),
+  divBoxes: document.querySelector("#boxes"),
+};
+
+const buttonCreate = elements.buttons[0];
+const buttonDestroy = elements.buttons[1];
+
+const input = elements.divControls.firstElementChild;
+
+buttonCreate.addEventListener("click", createBoxes);
+
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = input.value;
+
+  for (let i = 0; i < Number(amount); i += 1) {
+    let newEl = document.createElement("div");
+    newEl.style.backgroundColor = getRandomHexColor();
+    newEl.style.width = "30px";
+    newEl.style.height = "30px";
+    elements.divBoxes.append(newEl);
+  }
+}
+
+function destroyBoxes(amount) {
+  amount = input.value;
+
+  for (let i = 0; i < Number(amount); i += 1) {
+    elements.divBoxes.lastElementChild.remove();
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
