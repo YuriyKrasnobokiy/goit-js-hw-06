fc7f8ccbd5f77fc5639ee1c9b0ca65fa81c9d177
Yuriy Krasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2023-07-29 04:28:56 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const elements = {
+  button: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
+elements.button.addEventListener("click", handlerClick);
+
+function handlerClick() {
+  elements.body.style.backgroundColor = elements.color.textContent;
+  elements.color.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
